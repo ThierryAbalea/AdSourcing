9322a6d070f7e2a3f785151f2a77c81b941ffe9d
rbrugier <raphael.brugier@gmail.com>
2013-10-20 00:47:21 +0200

    Factorize color picker list

--- adsourcing-web/src/main/webapp/designer/js/controllers.js
+++ adsourcing-web/src/main/webapp/designer/js/controllers.js
@@ -48,8 +48,10 @@ angular.module('myApp.controllers', [])
         'advertiser': 'Sarenza',
 
         'description': 'La campagne hiver 2013 est Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ',
-
-
     }
+
+    $scope.bcgColors = ['#ee7722', '#ff5500', '#eeeeee', '#eecccc', '#997766', '#dd7744'];
+
+
 }])
 ;
