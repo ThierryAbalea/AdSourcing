4566da29a11e7571a2c08b88c4d4e2f7bee7bae9
rbrugier <raphael.brugier@gmail.com>
2013-10-19 20:50:14 +0200

    Add one more campaign.

--- adsourcing-service/src/main/java/com/adsourcing/service/repository/CampaignData.java
+++ adsourcing-service/src/main/java/com/adsourcing/service/repository/CampaignData.java
@@ -24,10 +24,10 @@ public class CampaignData {
     public List<Campaign> getCampaigns() {
         List<Campaign> campaigns = newArrayList();
 
-        Advertiser advertiser = this.advertiserRepository.findByName("Sarenza");
+        Advertiser sarenza = this.advertiserRepository.findByName("Sarenza");
 
         Campaign campaign1 = new Campaign();
-        campaign1.setAdvertiser(advertiser);
+        campaign1.setAdvertiser(sarenza);
         campaign1.setTitle("Fêtes de Noël");
         campaign1.setDescription("On veut que le Père Noël taffe dur cette année");
         campaign1.setTags(newHashSet("Fête", "Noël", "Hiver"));
@@ -37,7 +37,7 @@ public class CampaignData {
         campaigns.add(campaign1);
 
         Campaign campaign2 = new Campaign();
-        campaign2.setAdvertiser(advertiser);
+        campaign2.setAdvertiser(sarenza);
         campaign2.setTitle("Fêtes des Mères");
         campaign2.setDescription("Rendez Maman heureuse");
         campaign2.setTags(newHashSet("Fête", "Mère"));
@@ -45,6 +45,18 @@ public class CampaignData {
         campaign2.setStartDate(new Date(2014, 4, 1));
         campaign2.setEndDate(new Date(2014, 4, 15));
         campaigns.add(campaign2);
+
+        Advertiser cDiscount = this.advertiserRepository.findByName("CDiscount");
+
+        Campaign campaign3 = new Campaign();
+        campaign3.setAdvertiser(cDiscount);
+        campaign3.setTitle("High tech powa");
+        campaign3.setDescription("La grosse promotion annuelle de CDiscount sur le high tech.");
+        campaign3.setTags(newHashSet("High Tech", "Geek"));
+        campaign3.setBudget(10000);
+        campaign3.setStartDate(new Date(2014, 4, 1));
+        campaign3.setEndDate(new Date(2014, 4, 15));
+        campaigns.add(campaign3);
 
         return campaigns;
     }
