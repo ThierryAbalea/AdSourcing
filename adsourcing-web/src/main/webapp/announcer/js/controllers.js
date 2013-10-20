814622a5e3ad4fdd8e38c913ee99d50946f13cb0
fmauquie <fabien.mauquie@gmail.com>
2013-10-20 15:04:59 +0200

    Convert advertiser campaign to CDiscount

--- adsourcing-web/src/main/webapp/announcer/js/controllers.js
+++ adsourcing-web/src/main/webapp/announcer/js/controllers.js
@@ -78,11 +78,27 @@ function($scope, $location, createCampaignService) {
                 perf: 2.5,
                 reco: 2
             },
-            visual: '../img/bandeau-sarenza-1.jpg',
-            targeting: ['Women', 'Aged 30-50', 'From Any City']
+            visual: '../img/cdiscount-1.jpg',
+            targeting: ['Men & Women', 'Aged 15-25', 'From Cities']
         },
-        { user: {name: 'Stephan'} },
-        { user: {name: 'Morgane'} },
+        {
+            user: {
+                name: 'Stéphane',
+                perf: 1.5,
+                reco: 3
+            },
+            visual: '../img/cdiscount-2.jpg',
+            targeting: ['Men', 'Aged 25-35', 'From Everywhere']
+        },
+        {
+            user: {
+                name: 'Morgan',
+                perf: 2,
+                reco: 4
+            },
+            visual: '../img/cdiscount-3.jpg',
+            targeting: ['Men', 'Aged 15-35', 'From Cities']
+        },
         { user: {name: 'David'} },
         { user: {name: 'TheDude'} },
         { user: {name: 'John'} },
@@ -100,8 +116,8 @@ function($scope, $location, createCampaignService) {
             perf: 2,
             reco: 3.5
         },
-        visual: '../img/bandeau-sarenza-0.jpg',
-        targeting: ['Women', 'Aged 20-40', 'From Paris (incl. suburbs)']
+        visual: '../img/cdiscount-0.jpg',
+        targeting: ['Men & Women', 'Aged 18-50', 'From Cities and Suburbs']
     };
     
     $scope.fullStars = function(score) {
